2cebf76b43584b73d22dc2e95ab276e487621832
Dave <d4park@gmail.com>
2015-07-14 03:00:51 -0400

    Added config for database_cleaner gem and removed the valid_session variable for now.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -29,16 +29,14 @@ RSpec.describe UsersController, type: :controller do
   }
   
   let(:invalid_attributes) {
-    nil
+    {}
   }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # UsersController. Be sure to keep this updated too.
   let(:valid_session) {       
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:user)
-      sign_in user
+      # User is signed in using the ControllerMacros module.
   }
 
   describe "GET #index" do
